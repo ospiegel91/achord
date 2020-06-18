693f0ceb9bb8e252b82feb7d057e9b7165d5e65f
Oren Spiegel <ospiegel51191@gmail.com>
2020-06-18 22:10:59 +0300

    gg

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '1012989251676-rk2uabv4m6el5mmrujph0lihjcdlj6qk.apps.googleusercontent.com',
+    googleClientSecret: 'pCKePtJo14ptzKGIwtc7VQM1',
+    mongoURI: 'mongodb+srv://admin:xLHYLbxbcxO43lx9@achord-jyhxf.gcp.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'uhfD#@VgVqou^fsGkgbDJ!:'
+}
