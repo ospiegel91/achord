61f264fd0d17117e093660b035d08303cee43349
Oren Spiegel <ospiegel51191@gmail.com>
2020-06-20 23:22:05 +0300

    fixing deployment related vars

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-    googleClientID: '1012989251676-rk2uabv4m6el5mmrujph0lihjcdlj6qk.apps.googleusercontent.com',
-    googleClientSecret: 'pCKePtJo14ptzKGIwtc7VQM1',
-    mongoURI: 'mongodb+srv://admin:xLHYLbxbcxO43lx9@achord-jyhxf.gcp.mongodb.net/test?retryWrites=true&w=majority',
-    cookieKey: 'uhfD#@VgVqou^fsGkgbDJ!:'
+if (process.env.NODE_ENV == 'production'){
+    module.exports = require('./prod');
+}else{
+    module.exports = require('./dev');
 }
